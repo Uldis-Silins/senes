a3e3824749d1763c87ab761bf4853cbf1405fa77
Uldis-Silins <dissid@inbox.lv>
2019-06-08 02:39:20 +0300

    kamer vel ir

---
+++ SenesLegacy/Assets/Editor/PropSpawnControllerInspector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PropSpawnController))]
+public class PropSpawnControllerInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var propSpawner = (PropSpawnController)target;
+
+        DrawDefaultInspector();
+
+        if(GUILayout.Button("Spawn Shit"))
+        {
+            propSpawner.SpawnStuff();
+        }
+    }
+}
